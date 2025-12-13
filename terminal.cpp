c0e74e20d5c7f3c8a198d00d6bfa89bf031d291b
Daksh <dakshjain2316@gmail.com>
2025-12-13 18:09:56 +0530

    Heart and Shade function

--- terminal.cpp
+++ terminal.cpp
@@ -1,11 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
+double Heart(double x, double y, double r = 1.0) {
+    return pow(x*x + y*y - 1, 3) - r*x*x*pow(y, 3);
+}
+
+char Shade(double val) {
+    if(val <= 0.0) return '*';
+    return ' ';                   
+} 
+
 int main() {
     for(double y = 1.3;y >= -1.3;y-= 0.075){
         for(double x = -1.3;x <= 1.3;x += 0.03){
-           if(pow(x*x + y*y - 1, 3) - x*x*pow(y, 3) < 0) cout << '*';
-           else cout << ' ';
+           cout << Shade(Heart(x,y));
         }
         cout << endl;
     }
