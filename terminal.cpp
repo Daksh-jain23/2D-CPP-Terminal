972e8dd568d2d471ce14d522ba8e5671fc884a5f
Daksh <dakshjain2316@gmail.com>
2025-12-13 18:17:38 +0530

    Adding more functions

--- terminal.cpp
+++ terminal.cpp
@@ -5,15 +5,36 @@ double Heart(double x, double y, double r = 1.0) {
     return pow(x*x + y*y - 1, 3) - r*x*x*pow(y, 3);
 }
 
+double Circle(double x, double y, double r = 1.0) {
+    return (x*x)+(y*y)-(r*r);
+}
+
+double Ellipse(double x, double y, double a = 1.0, double b = 1){
+    return (x*x)/(a*a) + (y*y)/(b*b) - 1;
+}
+
+double Hyperbola(double x, double y, double a = 1.0, double b = 1.0){
+    return (x*x)/(a*a) - (y*y)/(b*b) - 1;
+}
+
+double Astroid(double x, double y, double a = 1.0, double b = 1.0){
+    return pow(x/a, 2.0/3.0) + pow(y/b, 2.0/3.0) - 1;
+}
+
+double LuckyLeaf(double x, double y, double r = 1.0){
+    return pow(x*x + y*y, 5) - r*r*pow(x*x - y*y, 2);
+}
+
 char Shade(double val) {
     if(val <= 0.0) return '*';
     return ' ';                   
-} 
+}                
+ 
 
 int main() {
     for(double y = 1.3;y >= -1.3;y-= 0.075){
         for(double x = -1.3;x <= 1.3;x += 0.03){
-           cout << Shade(Heart(x,y));
+           cout << Shade(Astroid(x,y));
         }
         cout << endl;
     }
